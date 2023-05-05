78c01518b532332772700b866fe8aa007646ba22
Lorodo <barrioleal1994@gmail.com>
2023-05-05 12:54:40 +0200

    modificacion para comprobar que el cron está funcionando

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("funciono")
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+
+dt_string = now.strftime("%Y%m%d_%H-%M-%S")
+
+f = open(dt_string+".txt", "x")
